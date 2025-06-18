06df5996c4bdde1e913ee3076ff8c24a236e02a8
Ettore Gigli <ettorinogigli02@gmail.com>
2025-06-18 17:13:44 +0200

    Structures file, first commit

--- service/structures/structures.go
+++ service/structures/structures.go
@@ -1,1 +1,37 @@
 package structures
+
+type User struct {
+	ID             int    `json:"id"`
+	Username       string `json:"username"`
+	DisplayName    string `json:"displayName"`
+	ProfilePicture string `json:"profilePicture"`
+}
+
+type Reaction struct {
+	ID    int    `json:"id"`
+	Emoji string `json:"emoji"`
+	User  User   `json:"user"`
+}
+
+type Message struct {
+	ID          int        `json:"id"`
+	Content     string     `json:"content"`
+	IsForwarded bool       `json:"isForwarded"`
+	MediaType   string     `json:"mediaType"`
+	Reactions   []Reaction `json:"reactions"`
+	Sender      User       `json:"sender"`
+	Status      string     `json:"status"`
+	Timestamp   string     `json:"timestamp"`
+}
+
+type Conversation struct {
+	ID           int     `json:"id"`
+	LastMessage  Message `json:"lastMessage"`
+	Participants []User  `json:"participants"`
+}
+
+type Group struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Members []User `json:"members"`
+}
